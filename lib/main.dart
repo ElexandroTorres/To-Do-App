2b06c3a8a13646c62c857b2d767e5b3410ff77bf
Elexandro Torres <elexandro.torres@gmail.com>
2022-01-11 16:30:40 -0300

    Main Screen implemented, as the simple bottom sheet to add a task

--- lib/main.dart
+++ lib/main.dart
@@ -18,10 +18,88 @@ class ToDoApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  Widget builtBottomSheet(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(32),
+          topRight: Radius.circular(32),
+        ),
+      ),
+      child: Column(
+        children: [
+          Text('Add task'),
+          TextField(),
+          TextButton(
+            onPressed: () {},
+            child: Text('Adicionar'),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(child: Icon(Icons.list)),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'My To-Do List',
+                    style: TextStyle(
+                        fontSize: 32,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  Text('2 tarefas restantes'),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(32),
+                    topRight: Radius.circular(32),
+                  ),
+                ),
+                child: ListView(
+                  children: [
+                    ListTile(
+                        title: Text('teste'),
+                        leading: Icon(Icons.h_mobiledata),
+                        trailing: Checkbox(
+                          onChanged: (value) => {},
+                          value: false,
+                        )),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        onPressed: () {
+          showModalBottomSheet(context: context, builder: builtBottomSheet);
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
